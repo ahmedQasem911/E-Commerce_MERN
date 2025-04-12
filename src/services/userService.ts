62447c2e4b9e987520bd6a132622ff63ef2a06f5
Ahmed Qasem <ahmed.o.qasem911@gmail.com>
2025-04-12 06:11:57 +0200

    Initial Commit + User Feature

---
+++ src/services/userService.ts
@@ -0,0 +1,73 @@
+import userModel from "../models/userModel";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+interface registerParams {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+
+// Register Function
+
+export const register = async ({
+  firstName,
+  lastName,
+  email,
+  password,
+}: registerParams) => {
+  const findUser = await userModel.findOne({ email });
+
+  if (findUser) {
+    return { data: "User already exists!", statusCode: 400 };
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new userModel({
+    firstName,
+    lastName,
+    email,
+    password: hashedPassword,
+  });
+  await newUser.save();
+
+  return { data: generateJWT({ firstName, lastName, email }), statusCode: 200 };
+};
+
+interface LoginParams {
+  email: string;
+  password: string;
+}
+
+// Login Function
+
+export const login = async ({ email, password }: LoginParams) => {
+  const findUser = await userModel.findOne({ email });
+
+  if (!findUser) {
+    return { data: "Incorrect Email or Password!", statusCode: 400 };
+  }
+
+  // const passwordMatch = password === findUser.password;
+  const passwordMatch = await bcrypt.compare(password, findUser.password);
+
+  if (passwordMatch) {
+    return {
+      data: generateJWT({
+        firstName: findUser.firstName,
+        lastName: findUser.lastName,
+        email,
+      }),
+      statusCode: 200,
+    };
+  }
+
+  return { data: "Incorrect Email or Password!", statusCode: 400 };
+};
+
+const generateJWT = (data: any) => {
+  return jwt.sign(data, "AkUoHofrunPE5E0vKGU7vqz0rtipgmHq", {
+    expiresIn: "24h",
+  });
+};
