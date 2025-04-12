37d2994eacb11e43111e1ab68c0f5550ba31ccf2
Ahmed Qasem <ahmed.o.qasem911@gmail.com>
2025-04-12 06:51:30 +0200

    Product Feature

---
+++ src/routes/productRoute.ts
@@ -0,0 +1,11 @@
+import express, { response } from "express";
+import { getAllProducts } from "../services/productService";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const products = await getAllProducts();
+  res.status(200).send(products);
+});
+
+export default router;
---
+++ src/services/productService.ts
@@ -0,0 +1,17 @@
+import productModel from "../models/productModel";
+
+export const getAllProducts = async () => {
+  return await productModel.find();
+};
+
+export const seedInitialProducts = async () => {
+  const products = [
+    { title: "MSI Laptop",  image: "https://storage-asset.msi.com/global/picture/image/feature/nb/GT/GT77-13V/images/kv-laptop.png",  price: 1000, stock: 23 },
+  ];
+
+  const existingProducts = await getAllProducts();
+
+  if(existingProducts.length === 0) {
+    await productModel.insertMany(products);
+  }
+};
