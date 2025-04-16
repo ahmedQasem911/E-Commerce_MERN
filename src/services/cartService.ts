c2024a530908882110199236f9006cf4b8267131
Ahmed Qasem <ahmed.o.qasem911@gmail.com>
2025-04-16 03:00:28 +0200

    Add item to cart /cart/items POST endpoint

--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,4 +1,5 @@
 import { cartModel } from "../models/cartModel";
+import productModel from "../models/productModel";
 
 interface CreateCartForUser {
   userId: string;
@@ -24,4 +25,48 @@ export const getActiveCartForUser = async ({
   }
 
   return cart;
+};
+
+interface AddItemToCart {
+  productId: any;
+  quantity: number;
+  userId: string;
+}
+
+export const addItemToCart = async ({
+  productId,
+  quantity,
+  userId,
+}: AddItemToCart) => {
+  const cart = await getActiveCartForUser({ userId });
+
+  const existsInCart = cart.items.find((p) => p.product.toString() === productId);
+
+  if (existsInCart) {
+    return { data: "Item already exists in cart!", statusCode: 400 };
+  }
+
+  // Fetch the product
+  const product = await productModel.findById(productId);
+
+  if (!product) {
+    return { data: "Product not found!", statusCode: 400 };
+  }
+
+  if (product.stock < quantity) {
+    return { data: "Low stock for item!", statusCode: 400 };
+  }
+
+  cart.items.push({
+    product: productId,
+    unitPrice: product.price,
+    quantity,
+  });
+
+  // Update total amount
+  cart.totalAmount += product.price * quantity;
+
+  const updatedCart = await cart.save();
+
+  return { data: updatedCart, statusCode: 201 };
 };
