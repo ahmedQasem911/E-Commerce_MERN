1139e063af6b31258badd78f1d558bd8ee2cfe61
Ahmed Qasem <ahmed.o.qasem911@gmail.com>
2025-04-19 22:03:15 +0200

    All Products in Home Page + API Integration

--- Backend/src/services/productService.ts
+++ Backend/src/services/productService.ts
@@ -11,8 +11,22 @@ export const seedInitialProducts = async () => {
         title: "MSI Laptop",
         image:
           "https://storage-asset.msi.com/global/picture/image/feature/nb/GT/GT77-13V/images/kv-laptop.png",
-        price: 1000,
+        price: 1200,
         stock: 23,
+      },
+      {
+        title: "ASUS Laptop",
+        image:
+          "https://www.asus.com/media/Odin/Websites/global/ProductLine/20200824120814.jpg",
+        price: 3500,
+        stock: 8,
+      },
+      {
+        title: "Huawei Laptop",
+        image:
+          "https://consumer.huawei.com/dam/content/dam/huawei-cbg-site/me-africa/common/mkt/plp/laptops-new/matebook-x-series.jpg",
+        price: 900,
+        stock: 14,
       },
     ];
 
--- Frontend/src/pages/HomePage.tsx
+++ Frontend/src/pages/HomePage.tsx
@@ -1,5 +1,47 @@
+import { Box, Container, Grid } from "@mui/material";
+import ProductCard from "../components/ProductCard";
+import { useEffect, useState } from "react";
+import { Product } from "../types/Product";
+import { BASE_URL } from "../constants/baseURL";
+
 const HomePage = () => {
-  return <div>Home Page</div>;
+  const [products, setProducts] = useState<Product[]>([]);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BASE_URL}/product`);
+        const data = await response.json();
+        setProducts(data);
+      } catch {
+        setError(true);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (error) {
+    return <Box>Something went wrong, please try again!</Box>;
+  }
+
+  return (
+    <>
+      <Container sx={{ mt: 3 }}>
+        <Grid container spacing={3}>
+          {products.map((p) => (
+            <Grid
+              sx={{ marginLeft: "auto", marginRight: "auto" }}
+              minWidth={"30%"}
+            >
+              <ProductCard {...p} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </>
+  );
 };
 
 export default HomePage;
