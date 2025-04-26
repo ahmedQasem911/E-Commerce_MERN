9934e29026b5d7f948bd0c14aed4f599e0988988
Ahmed Qasem <ahmed.o.qasem911@gmail.com>
2025-04-26 17:28:15 +0300

    Cart Page Layout

--- Frontend/src/pages/CartPage.tsx
+++ Frontend/src/pages/CartPage.tsx
@@ -1,22 +1,47 @@
-import { Box, Container, Typography } from "@mui/material";
-import { useEffect, useState } from "react";
-import { BASE_URL } from "../constants/baseURL";
-import { useAuth } from "../context/Auth/AuthContext";
+import { Box, Button, ButtonGroup, Container, Typography } from "@mui/material";
 import { useCart } from "../context/Cart/CartContext";
 
 const CartPage = () => {
-  const { token } = useAuth();
   const { cartItems, totalAmount } = useCart();
-  const [error, setError] = useState("");
-
-  
 
   return (
-    <Container sx={{ mt: 2 }}>
+    <Container fixed sx={{ mt: 2 }}>
       <Typography variant="h4">My Cart</Typography>
-      {cartItems.map((item) => (
-        <Box>{item.title}</Box>
-      ))}
+      <Box display="flex" flexDirection="column" gap={1}>
+        {cartItems.map((item) => (
+          <Box
+            display="flex"
+            flexDirection="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{
+              mt: 2,
+              border: 1,
+              borderColor: "#909090",
+              borderRadius: 5,
+              padding: 2,
+            }}
+          >
+            <Box display="flex" flexDirection="row" alignItems="center" gap={5}>
+              <img src={item.image} alt="product-image" width={100} />
+              <Box>
+                <Typography variant="h6">{item.title}</Typography>
+                <Typography>
+                  {item.quantity} × {item.unitPrice}$
+                </Typography>
+                <Button>Remove Item</Button>
+              </Box>
+            </Box>
+            <ButtonGroup variant="contained" aria-label="Basic button group">
+              <Button>-</Button>
+              <Button>+</Button>
+            </ButtonGroup>
+          </Box>
+        ))}
+        <Box sx={{ mt: 3 }}>
+          <Typography variant="h5">Total Amount: {totalAmount}$</Typography>
+        </Box>
+      </Box>
     </Container>
   );
 };
